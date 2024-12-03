85d89b77e8ba8565bfdbcc0f4eecf158be2bc44f
PatrickFish-8 <patrickfish10@icloud.com>
2024-12-03 18:55:24 -0500

    API integration
    Google Gemini integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,31 @@
+import Navbar from './components/navbar/navbar'
+import Profile from './components/profile/profile'
+import Project from './components/projects/projects.jsx';
+import Experiences from './components/experiences/experiences.jsx';
+import About from './components/about/about.jsx';
+import Chat from './components/chat/chat.jsx';
+import './App.css'
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Navbar/>
+      <Profile/>
+      <Chat/>
+      <About/>
+      <div id="projects">
+        <h2>Projects</h2>
+        <Project name="Project 1" description="Project 1 Description" github="https://github.com"/>
+        <Project name="Project 2" description="Project 2 Description" github="https://github.com"/>
+      </div>
+      <div id='exp'>
+        <h2>Experience</h2>
+        <Experiences title="experience 1" info="info" date="date" bullets={['Bullet 1', 'Bullet 2']}/>
+        <Experiences title="experience 2" info="info" date="date" bullets={['Bullet 1', 'Bullet 2']}/>
+      </div>
+    </>
+    
   )
 }
 
